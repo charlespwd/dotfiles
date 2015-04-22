86e89d63fc86b93bf8458dfa3d0f26e01ed0f59b
Charles-P. Clermont <charles.pclermont@gmail.com>
2015-04-22 10:18:32 -0400

    Refactor install.sh

--- install.sh
+++ install.sh
@@ -10,43 +10,49 @@ create_backup_folder () {
 }
 
 backup_file () {
-  local file=$1
-  local current_dir=$2
+  local file="$1"
+  local current_dir="$2"
 
   if [[ -f ${file} ]]; then
     echo "backing up file ${file} to ${current_dir}/backup..."
-    mv ${file} "${current_dir}/backup"
+    mv "${file}" "${current_dir}/backup"
+    return "$?"
   elif [[ -d ${file} ]]; then
     echo "backing up directory ${file} to ${current_dir}/backup..."
-    mv ${file} "${current_dir}/backup"
+    mv "${file}" "${current_dir}/backup"
+    return "$?"
   fi
 }
 
 install_file () {
-  local file=$1
-  local current_dir=$(pwd)
+  local file="$1"
+  local current_dir="$2"
   local dot_file_path="${current_dir}/${file}"
-
-  cd ~
 
   # We don't want to install on top of symlinks
   if [[ -L ${file} ]]; then
     echo "${file} is already installed."
   else
     backup_file "${file}" "${current_dir}"
+    if [[ "$?" -ne 0 ]]; then
+      echo "Could not backup ${file}."
+      return 1
+    fi
+
     echo "installing ${file}"
     ln -s "${dot_file_path}" "~/${file}"
   fi
-
-  cd "${current_dir}"
 }
 
 main () {
-  readonly DOT_FILES=".*"
+  local DOT_FILES=".*"
+  local current_dir
+  current_dir="$(pwd)" || return
 
   create_backup_folder
   for dot_file in ${DOT_FILES}; do
-    install_file ${dot_file}
+    cd ~
+    install_file "${dot_file}" "${current_dir}"
     cd "${current_dir}"
   done
 }
