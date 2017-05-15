898c564e96d9a293353a3bdde1b7a9897547ebae
Charles-P. Clermont <charles@cpclermont.com>
2017-05-15 09:49:14 -0400

    i3 config update for 4k display

--- arch.sh
+++ arch.sh
@@ -1,8 +1,7 @@
-#
 # Nodejs stuff
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # this loads nvm
-source /usr/share/nvm/init-nvm.sh
+[ -s "/usr/share/nvm/init-nvm.sh" ] \
+  && source /usr/share/nvm/init-nvm.sh \
+  || echo "'nvm' not installed"
 
 # Startx on boot
 if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
@@ -14,11 +13,27 @@ export QT_QPA_PLATFORMTHEME='gtk3'
 export GDK_SCALE=2
 export GDK_DPI_SCALE=0.5
 
-# ssh key chain
-eval $(keychain --eval --quiet id_rsa)
+# Lastpass (don't quit) I'm auto logged into chrome anyway
+export LPASS_AGENT_TIMEOUT=0
+
+# Android home
+export ANDROID_HOME=/opt/android-sdk
+export PATH=${PATH}:${ANDROID_HOME}/tools/bin
+export PATH=${PATH}:${ANDROID_HOME}/platform-tools
+
+# ssh key chain (see .xinitrc)
+if [ -n "$DESKTOP_SESSION" ]; then
+    eval $(gnome-keyring-daemon --start)
+    export SSH_AUTH_SOCK
+fi
 
 # Load autoenv
-source /usr/share/autoenv/activate.sh
+[ -s "/usr/share/autoenv/activate.sh" ] \
+  && source /usr/share/autoenv/activate.sh \
+  || echo "'autoenv' not installed"
 
 # load ruby
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+# default browser
+export BROWSER='chromium'
