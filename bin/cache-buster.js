9a5e36e9fb1197110fdd19764f64189c79598f79
Charles-P. Clermont <charles@cpclermont.com>
2019-11-19 08:08:16 -0500

    Add nginx cache buster server

---
+++ bin/cache-buster.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const { promisify } = require('util');
+const exec = require('child_process').exec;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  exec(`rm -rf /tmp/nginx-cache`, (err, resp) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('cleared cache');
+    }
+    res.end('cache cleared');
+  });
+});
+
+server.on('clientError', (err, socket) => {
+  socket.end('HTTP/1.1 400 Bad Request');
+});
+
+server.listen(8888);
