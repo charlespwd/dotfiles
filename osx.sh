bf6de90dc1100c947e0d449e82ce07392a5ddac4
Charles-P. Clermont <charles@cpclermont.com>
2021-04-11 08:47:30 -0400

    Update osx shell rc

--- osx.sh
+++ osx.sh
@@ -1,5 +1,5 @@
 # Path stuff
-export PATH="/opt/local/bin:/opt/local/sbin:$HOME/bin:$HOME/npm/bin:/usr/local/heroku/bin:/usr/local/bin:/bin:/usr/sbin:/sbin:/usr/bin:/usr/texbin"
+# export PATH="/opt/local/bin:/opt/local/sbin:$HOME/bin:$HOME/npm/bin:/usr/local/heroku/bin:/usr/local/bin:/bin:/usr/sbin:/sbin:/usr/bin:/usr/texbin"
 
 # RBENV config
 export RBENV_ROOT="$HOME/.rbenv"
@@ -9,12 +9,11 @@ if [ -d $RBENV_ROOT ]; then
   eval "$(rbenv init -)"
 fi
 
-# PYENV config
-export PYENV_ROOT="/usr/local/opt/pyenv"
+# Lastpass (don't quit) I'm auto logged into chrome anyway
+export LPASS_AGENT_TIMEOUT=0
 
-if which pyenv > /dev/null; then
-  eval "$(pyenv init -)";
-fi
+# Turn off fzf tmux. Looks like it doens't work on macOS
+export FZF_TMUX=0
 
 # BOOT config
 export BOOT_CLOJURE_VERSION="1.7.0"
@@ -27,8 +26,35 @@ alias cljsbuild="lein trampoline cljsbuild $@"
 
 # Node config
 export NVM_DIR=~/.nvm
-source $(brew --prefix nvm)/nvm.sh
+nvm_src="$(brew --prefix nvm)/nvm.sh"
+if [ -d $nvm_src ]; then
+  source "$nvm_src"
+fi
 
-### Added by the Heroku Toolbelt
-export PATH="/usr/local/heroku/bin:$PATH"
-export PHANTOMJS_BIN=`which phantomjs`
+# Load direnv
+if [[ $(whoami) = 'charles' ]]; then
+  if [[ -n "$ZSH_VERSION" ]]; then
+    eval "$(direnv hook zsh)"
+  elif [[ -n "$BASH_VERSION" ]]; then
+    eval "$(direnv hook bash)"
+  fi
+fi
+
+# PYENV config
+# export PYENV_ROOT="/usr/local/opt/pyenv"
+
+# if which pyenv > /dev/null; then
+#   eval "$(pyenv init -)";
+# fi
+
+# # Load python
+# if command -v pyenv 1>/dev/null 2>&1; then
+#   eval "$(pyenv init -)"
+# fi
+
+# load docker machine
+if command -v docker-machine &>/dev/null && docker info &>/dev/null; then
+  eval $(docker-machine env default)
+fi
+
+#vim:ft=zsh
